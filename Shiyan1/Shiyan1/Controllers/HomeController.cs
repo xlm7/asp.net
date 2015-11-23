f8add57fd663c4faf25f9d101dba83ddc84f912b
xlm7 <1182789409@qq.com>
2015-11-23 15:00:12 +0800

    asp.net实验一的代码

---
+++ Shiyan1/Shiyan1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shiyan1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Show()
+        {
+            ViewBag.Message = "show.";
+
+            return View();
+        }
+        public ActionResult Test()
+        {
+            ViewBag.Message = "show.";
+
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public string CheckUsername(string username)
+        {
+            if (username.Equals("wustzz"))
+            {
+                return "已注册";
+            }
+            else
+            {
+                return "检查通过";
+            }
+        }
+    }
+}
---
+++ Shiyan1/Shiyan1/Controllers/InforController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shiyan1.Controllers
+{
+    public class InforController : Controller
+    {
+        // GET: Infor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Show()
+        {
+            ViewBag.Message = "Show.";
+
+            return View();
+        }
+
+        public string CheckUsername(string username)
+        {
+            if (username.Equals("wustzz"))
+                return "已注册";
+            else return "检查通过";
+        }
+    }
+}
