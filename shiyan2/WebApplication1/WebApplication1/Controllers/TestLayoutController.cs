59c2860e8452c720f6ad9cecf52459ad849f0605
xlm7 <1182789409@qq.com>
2015-11-28 19:56:27 +0800

    提交.net实验2,在线花店

---
+++ shiyan2/WebApplication1/WebApplication1/Controllers/TestLayoutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestLayoutController : Controller
+    {
+        // GET: TestLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+       
+    }
+}
